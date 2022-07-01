174295b2beffde15076b0ff78d89dbba339afdca
Andrew Kovalenko <andrew8999@gmail.com>
2022-07-01 12:03:43 +0300

    Update Program.cs

--- IJuniorHomeWork/Program.cs
+++ IJuniorHomeWork/Program.cs
@@ -6,7 +6,14 @@ namespace IJuniorHomeWork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int step = 7;
+            int start = 5;
+            int end = 100;
+
+            for (int i = start; i < end; i += step)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
