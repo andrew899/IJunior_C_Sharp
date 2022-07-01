3c67bc83a3a8c51b50c4c28d2f3ce4ed197b9248
Andrew Kovalenko <andrew8999@gmail.com>
2022-07-01 12:29:07 +0300

    Update Program.cs

--- IJuniorHomeWork/Program.cs
+++ IJuniorHomeWork/Program.cs
@@ -6,7 +6,22 @@ namespace IJuniorHomeWork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random random = new Random();
+
+            int number = random.Next(100);
+            Console.WriteLine($"Number = {number}");
+
+            int sum = 0;
+            for (int i = 0; i <= number; i++)
+            {
+                if (i%2 == 0 && (i%3 == 0 || i%5 == 0))
+                {
+                    Console.WriteLine(i);
+                    sum += i; 
+                }
+            }
+
+            Console.WriteLine($"Sum = {sum} ");
         }
     }
 }
