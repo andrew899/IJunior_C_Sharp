f4bbf8c9780a558242831625b59707b537831b52
Andrew Kovalenko <andrew8999@gmail.com>
2022-07-01 12:30:40 +0300

    Update Program.cs

--- IJuniorHomeWork/Program.cs
+++ IJuniorHomeWork/Program.cs
@@ -14,7 +14,7 @@ namespace IJuniorHomeWork
             int sum = 0;
             for (int i = 0; i <= number; i++)
             {
-                if (i%2 == 0 && (i%3 == 0 || i%5 == 0))
+                if (i%3 == 0 || i%5 == 0)
                 {
                     Console.WriteLine(i);
                     sum += i; 
