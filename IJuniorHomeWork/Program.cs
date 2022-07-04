d65827c92900690e31d3379128c77d51ec120796
Andrew Kovalenko <andrew8999@gmail.com>
2022-07-04 17:23:21 +0300

    Update Program.cs

--- IJuniorHomeWork/Program.cs
+++ IJuniorHomeWork/Program.cs
@@ -6,7 +6,23 @@ namespace IJuniorHomeWork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random random = new Random();
+            int number = random.Next();
+
+            int exponentNumber = 2;
+            int exponentIndex = 1;
+            int exponentResult = 2;
+
+            while (exponentResult <= number)
+            {
+                exponentResult *= exponentNumber;
+                exponentIndex += 1;
+            }
+
+            Console.WriteLine($"Number: {number}");
+            Console.WriteLine($"Exponent index: {exponentIndex}");
+            Console.WriteLine($"Exponent result: {exponentResult}");
+
         }
     }
 }
