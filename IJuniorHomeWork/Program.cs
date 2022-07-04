734a62031292778061a2c42230c69fca2242b758
Andrew Kovalenko <andrew8999@gmail.com>
2022-07-04 17:12:20 +0300

    Update Program.cs

--- IJuniorHomeWork/Program.cs
+++ IJuniorHomeWork/Program.cs
@@ -6,7 +6,23 @@ namespace IJuniorHomeWork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random random = new Random();
+            int min = 1;
+            int max = 28;
+            int number = random.Next(min, max);
+            int amountOfMultiplies = 0;
+            int length = 1000;
+            
+            for (int i = 0; i < length; i += number)
+            {
+                if(i >= 100)
+                {
+                    amountOfMultiplies += 1;
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine($"Number: {number}, has {amountOfMultiplies} amount of 3-digit multiplies");
         }
     }
 }
