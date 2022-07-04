e4dc2f90b223a9fc8e2d17dae20726381a9b2c03
Andrew Kovalenko <andrew8999@gmail.com>
2022-07-04 18:02:49 +0300

    Update Program.cs

--- IJuniorHomeWork/Program.cs
+++ IJuniorHomeWork/Program.cs
@@ -6,7 +6,49 @@ namespace IJuniorHomeWork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string bracketExpression = "(()(()))";
+            //string bracketExpression = ")(";
+            //string bracketExpression = "(()))(()";
+            //string bracketExpression = "(()";
+            //string bracketExpression = "())";
+            char bracketOpen = '(';
+            char bracketClose = ')';
+            int bracketOpenAmount = 0;
+            int bracketCloseAmount = 0;
+            int maxDepth = 0;
+            int depth = 0;
+
+            foreach (var item in bracketExpression)
+            {
+                if (bracketCloseAmount > bracketOpenAmount)
+                {
+                    break;
+                }
+                else if(item.Equals(bracketOpen))
+                {
+                    bracketOpenAmount++;
+                    depth++;
+                }
+                else if (item.Equals(bracketClose))
+                {
+                    bracketCloseAmount++;
+                    if(maxDepth < depth)
+                    {
+                        maxDepth = depth;
+                    }
+                    depth = 1;
+                }
+            }
+
+            if(bracketOpenAmount == bracketCloseAmount && maxDepth > 0)
+            {
+                Console.WriteLine("Bracket expresion " + bracketExpression + " is correct.");
+                Console.WriteLine($"Depth: {maxDepth}");
+            }
+            else
+            {
+                Console.WriteLine("Bracket expresion " + bracketExpression + " is incorrect.");
+            }
         }
     }
 }
