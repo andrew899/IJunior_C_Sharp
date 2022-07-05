7c25a69f0b343a667f531d354e315662877bfa3b
Andrew Kovalenko <andrew8999@gmail.com>
2022-07-05 11:43:54 +0300

    Update Program.cs

--- IJuniorHomeWork/Program.cs
+++ IJuniorHomeWork/Program.cs
@@ -6,7 +6,30 @@ namespace IJuniorHomeWork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter char for border: ");
+            char borderChar = Console.ReadKey().KeyChar;
+            Console.Write("\nEnter user string: ");
+            string userString = Console.ReadLine();
+
+            int amountOfStrings = 3;
+            int stringNumberForUserString = 2;
+            int borderLenth = 4;
+
+            int lenthUserString = userString.Length;
+            int stringLenth = borderLenth + lenthUserString;
+            string borderString = new string(borderChar, stringLenth);
+
+            for (int i = 1; i <= amountOfStrings; i++)
+            {
+                if (i == stringNumberForUserString)
+                {
+                    Console.WriteLine(borderChar + " " + userString + " " + borderChar);
+                }
+                else
+                {
+                    Console.WriteLine(borderString);
+                }
+            }
         }
     }
 }
