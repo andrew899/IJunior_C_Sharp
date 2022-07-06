f79ce1b635d053d382403af34f5fb0fe4c80a5e1
Andrew Kovalenko <andrew8999@gmail.com>
2022-07-06 14:58:30 +0300

    Update Program.cs

--- IJuniorHomeWork/Program.cs
+++ IJuniorHomeWork/Program.cs
@@ -6,7 +6,31 @@ namespace IJuniorHomeWork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int arraySize = 3;
+
+            int numberOfLine = 1;
+            int numberOfColumn = 0;
+            int sum = 0;
+            int resultMultiplication = 1;
+
+            int[,] ints = { { 1, 1, 1 }, 
+                            { 1, 1, 1 }, 
+                            { 1, 1, 1 } };
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                for (int j = 0; j < arraySize; j++)
+                {
+                    sum += ints[numberOfLine,j];
+
+                    Console.Write(ints[i, j] + " ");
+                }
+                resultMultiplication *= ints[i,numberOfColumn];
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"Sum: {sum}.");
+            Console.WriteLine($"Multiplication: {resultMultiplication}");
         }
     }
 }
