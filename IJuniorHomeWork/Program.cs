302bed259934e62bac2ac2d90486bcfa633100eb
Andrew Kovalenko <andrew8999@gmail.com>
2022-07-06 15:55:41 +0300

    Update Program.cs

--- IJuniorHomeWork/Program.cs
+++ IJuniorHomeWork/Program.cs
@@ -15,16 +15,20 @@ namespace IJuniorHomeWork
                             { 1, 1, 1 }, 
                             { 7, 8, 9 } };
 
+            for (int i = 0; i < ints.GetLength(1); i++)
+            {
+                sum += ints[numberOfLine,i];
+            }
+
+            for (int i = 0; i < ints.GetLength(0); i++)
+            {
+                resultMultiplication *= ints[i, numberOfColumn];
+            }
+
             for (int i = 0; i < ints.GetLength(0); i++)
             {
                 for (int j = 0; j < ints.GetLength(1); j++)
                 {
-                    if (j == numberOfColumn)
-                        resultMultiplication *= ints[i,j];
-
-                    if (i == numberOfLine)
-                        sum += ints[i,j];
-
                     Console.Write(ints[i, j] + " ");
                 }
                 Console.WriteLine();
