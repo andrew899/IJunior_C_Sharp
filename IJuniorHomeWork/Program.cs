46e5f99f89cc007f8b85eb950526d8f804e83992
Andrew Kovalenko <andrew8999@gmail.com>
2022-07-07 15:46:32 +0300

    Update Program.cs

--- IJuniorHomeWork/Program.cs
+++ IJuniorHomeWork/Program.cs
@@ -9,24 +9,21 @@ namespace IJuniorHomeWork
             int[] ints = new int[] {1, 2, 3, 4, 5 };
 
             PrintArray(ints);
-            var intsShufeled = Shufle(ints);
-            PrintArray(intsShufeled);
+            Shufle(ints);
+            PrintArray(ints);
         }
 
-        private static int[] Shufle(int[] ints)
+        private static void Shufle(int[] ints)
         {
             Random random = new Random();
-            int[] tempArray = ints;
 
-            for (int i = tempArray.Length - 1; i >= 1 ; i--)
+            for (int i = ints.Length - 1; i >= 1 ; i--)
             {
                 int randomPosition = random.Next(i + 1);
                 int tempNumber = ints[randomPosition];
                 ints[randomPosition] = ints[i];
                 ints[i] = tempNumber;
             }
-
-            return tempArray;
         }
 
         private static void PrintArray(int[] ints)
