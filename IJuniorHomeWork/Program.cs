c68f560ac93b31f4b4fdde1a642710070fc0295d
Andrew Kovalenko <andrew8999@gmail.com>
2022-07-07 14:30:40 +0300

    Update Program.cs

--- IJuniorHomeWork/Program.cs
+++ IJuniorHomeWork/Program.cs
@@ -6,7 +6,42 @@ namespace IJuniorHomeWork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] ints = new int[] { 1, 2, 3, 4 };
+            int amountOfShift = 0;
+
+            Console.WriteLine("Original array: ");
+            foreach (int element in ints)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Enter amount of shifts: ");
+            
+            while(Int32.TryParse(Console.ReadLine(), out amountOfShift) == false)
+            {
+                Console.Write("Incorrect number. Try again: ");
+            }          
+
+            while (amountOfShift-- > 0)
+            {
+                int firstElement = ints[0];
+
+                for (int i = 0; i < ints.Length - 1; i++)
+                {
+                    ints[i] = ints[i + 1];
+                }
+
+                ints[ints.Length - 1] = firstElement;
+
+                foreach (int element in ints)
+                {
+                    Console.Write(element + " ");
+                }
+                Console.WriteLine();
+                
+                Console.WriteLine($"Shift left: {amountOfShift}.");
+            }
         }
     }
 }
