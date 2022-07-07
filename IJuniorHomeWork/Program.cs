7b2e05f47eb2329ac793b0a29b23e7838dad27b9
Andrew Kovalenko <andrew8999@gmail.com>
2022-07-07 13:06:08 +0300

    Update Program.cs

--- IJuniorHomeWork/Program.cs
+++ IJuniorHomeWork/Program.cs
@@ -6,7 +6,38 @@ namespace IJuniorHomeWork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numbers = new int[] { 5, 5, 9, 9, 9, 5, 5 };
+            int maxRepeat = 1;
+            int maxRepeatTemp = 1;
+            int number = numbers[0];
+            int numberTemp = numbers[0];
+
+            for (int i = 1; i < numbers.Length - 1; i++)
+            {
+                if (numberTemp == numbers[i])
+                {
+                    maxRepeatTemp++;
+                }
+                else
+                {
+                    if (maxRepeatTemp > maxRepeat)
+                    {
+                        maxRepeat = maxRepeatTemp;
+                        maxRepeatTemp = 1;
+                        number = numberTemp;
+                        numberTemp = numbers[i];
+                    }
+                }
+            }
+
+            foreach (int i in numbers)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine($"Number: {number}.");
+            Console.WriteLine($"Max repeat: {maxRepeat}.");
         }
     }
 }
