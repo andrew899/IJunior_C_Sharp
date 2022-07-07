6104c761b360c1ec6b0e9fb6d0223a224d7d3ee9
Andrew Kovalenko <andrew8999@gmail.com>
2022-07-07 15:00:19 +0300

    Update Program.cs

--- IJuniorHomeWork/Program.cs
+++ IJuniorHomeWork/Program.cs
@@ -6,7 +6,29 @@ namespace IJuniorHomeWork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int fullHealth = 10;
+            int health = 4;
+
+            PrintHealth(health, fullHealth);
+        }
+
+        private static void PrintHealth(int health, int fullHealth)
+        {
+            Console.Write("Health [");
+
+            while (fullHealth-- >= 0)
+            {
+                if (health-- >= 0)
+                {
+                    Console.Write("#");
+                }
+                else
+                {
+                    Console.Write("_");
+                }
+            }
+            
+            Console.Write("]");
         }
     }
 }
