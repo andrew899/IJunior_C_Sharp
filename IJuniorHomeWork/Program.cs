641df682b6a65f0795f6ff2960c3e51a40febad6
Andrew Kovalenko <andrew8999@gmail.com>
2022-07-07 14:06:39 +0300

    Update Program.cs

--- IJuniorHomeWork/Program.cs
+++ IJuniorHomeWork/Program.cs
@@ -6,7 +6,41 @@ namespace IJuniorHomeWork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random random = new Random();
+            int maxRandom = 100;
+            int arraySize = 30;
+            int[] ints = new int[arraySize];
+
+            for (int i = 0; i < ints.Length; i++)
+            {
+                ints[i] = random.Next(maxRandom);
+            }
+
+            Console.WriteLine("Unsorted: ");
+            foreach (int number in ints)
+            {
+                Console.Write(number + " ");
+            }
+
+            for (int i = 0; i < ints.Length; i++)
+            {
+                for (int j = 0; j < ints.Length - 1; j++)
+                {
+                    if (ints[j] > ints[j + 1])
+                    {
+                        int tempNumber = ints[j + 1];
+                        ints[j + 1] = ints[j];
+                        ints[j] = tempNumber;
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Bubble sort: ");
+            foreach (int number in ints)
+            {
+                Console.Write(number + " ");
+            }
         }
     }
 }
