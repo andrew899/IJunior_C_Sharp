507d4f8e1f3653890b7f1a8b0b6ecbc7fd89232c
Andrew Kovalenko <andrew8999@gmail.com>
2022-07-12 16:42:23 +0300

    Update Program.cs

--- IJuniorHomeWork/Program.cs
+++ IJuniorHomeWork/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace IJuniorHomeWork
 {
@@ -6,7 +7,54 @@ namespace IJuniorHomeWork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string userInput = "";
+            List<int> numbers = new List<int>();
+            bool exit = false;
+
+            while (exit == false)
+            {
+                Console.WriteLine("Enter number to add to array.\n" +
+                                  "Enter SUM to get sum of all numbers in array.\n" +
+                                  "Enter EXIT to close program.");
+                userInput = Console.ReadLine().ToLower();
+
+                switch (userInput)
+                {
+                    case "sum":
+                        {
+                            int sum = 0;
+                            foreach (int number in numbers)
+                            {
+                                sum += number;
+                            }
+
+                            Console.WriteLine($"Sum: {sum}");
+                            break;
+                        }
+                    case "exit":
+                        {
+                            exit = true;
+                            break;
+                        }
+                    default:
+                        {
+                            int inputNumber = 0;
+
+                            if (Int32.TryParse(userInput, out inputNumber))
+                            {
+                                numbers.Add(inputNumber);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Unknown input. Try again.");
+                            }
+
+                            break;
+                        }
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
