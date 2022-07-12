a92b64e5838576e6540df442ad4d405e7c0e18d7
Andrew Kovalenko <andrew8999@gmail.com>
2022-07-12 16:24:14 +0300

    Update Program.cs

--- IJuniorHomeWork/Program.cs
+++ IJuniorHomeWork/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace IJuniorHomeWork
 {
@@ -6,7 +7,26 @@ namespace IJuniorHomeWork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Queue<int> clientsSumsToPay = new Queue<int>();
+            int shopAccount = 0;
+
+            clientsSumsToPay.Enqueue(100);
+            clientsSumsToPay.Enqueue(200);
+            clientsSumsToPay.Enqueue(150);
+            clientsSumsToPay.Enqueue(130);
+
+            while (clientsSumsToPay.Count != 0)
+            {
+                Console.WriteLine($"Shop account: {shopAccount}.");
+                int clientSum = clientsSumsToPay.Dequeue();
+                Console.WriteLine($"Client pays: {clientSum}.");
+                shopAccount += clientSum;
+                Console.WriteLine($"Enter any key to proceed to next client. Client left: {clientsSumsToPay.Count}.");
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+            Console.WriteLine($"Queue end. Shop account {shopAccount}.");
         }
     }
 }
