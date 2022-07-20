1f171c350b5f49f0311d82244c10caf55e98dc40
Andrew Kovalenko <andrew8999@gmail.com>
2022-07-20 09:45:24 +0300

    Update

--- IJuniorHomeWork/Program.cs
+++ IJuniorHomeWork/Program.cs
@@ -10,9 +10,21 @@ namespace IJuniorHomeWork
             var surname = "Pupkin";
             Player player = new Player(name, surname);
 
-            Console.WriteLine($"Player name: {player.GetName()}.");
-            Console.WriteLine($"Player surname: {player.GetSurname()}.");
-            Console.WriteLine($"Player name: {player.GetFullName()}.");
+            Console.WriteLine($"Player fullname: {player.GetFullName()}.");
         }
+    }
+
+    class Player
+    {
+        private string _name;
+        private string _surname;
+
+        public Player(string name, string surname)
+        {
+            _name = name;
+            _surname = surname;
+        }
+
+        public string GetFullName() => _name + " " + _surname;
     }
 }
