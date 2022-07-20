26fd20d8e32ec60689750923fe57989c42812ff5
Andrew Kovalenko <andrew8999@gmail.com>
2022-07-20 09:36:52 +0300

    Player class

---
+++ IJuniorHomeWork/Player.cs
@@ -0,0 +1,18 @@
+﻿namespace IJuniorHomeWork
+{
+    internal class Player
+    {
+        private string _name;
+        private string _surname;
+
+        public Player(string name, string surname)
+        {
+            this._name = name;
+            this._surname = surname;
+        }
+
+        public string GetName() { return _name; }
+        public string GetSurname() { return _surname; }
+        public string GetFullName() { return _name + " " + _surname; }
+    }
+}
--- IJuniorHomeWork/Program.cs
+++ IJuniorHomeWork/Program.cs
@@ -6,7 +6,13 @@ namespace IJuniorHomeWork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var name = "Vasya";
+            var surname = "Pupkin";
+            Player player = new Player(name, surname);
+
+            Console.WriteLine($"Player name: {player.GetName()}.");
+            Console.WriteLine($"Player surname: {player.GetSurname()}.");
+            Console.WriteLine($"Player name: {player.GetFullName()}.");
         }
     }
 }
