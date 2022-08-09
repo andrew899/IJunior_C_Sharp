c43f3021b3eeccedd05dc96543120981e7ff96c1
Andrew Kovalenko <andrew8999@gmail.com>
2022-08-09 12:33:11 +0300

    Update Program.cs

--- IJuniorHomeWork/Program.cs
+++ IJuniorHomeWork/Program.cs
@@ -7,61 +7,187 @@ namespace IJuniorHomeWork
     {
         static void Main(string[] args)
         {
+            Player player = new Player();
             DeckOfCards deck = new DeckOfCards();
 
-            Console.ReadLine();
+            bool isWorking = true;
+
+            while (isWorking)
+            {
+                Console.WriteLine("1. Take one card from deck.");
+                Console.WriteLine("2. Take few card from deck.");
+                Console.WriteLine("3. Show cards in hand.");
+                Console.WriteLine("4. Exit.");
+
+                var userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "1":
+                        player.AddCard(deck);
+                        break;
+
+                    case "2":
+                        player.AddCard(deck, "few");
+                        break;
+
+                    case "3":
+                        player.PrintAllCards();
+                        break;
+
+                    case "4":
+                        isWorking = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Wrong input.");
+                        break;
+                }
+            }
+        }
+    }
+
+    internal class Player
+    {
+        private List<Card> _cardInHand = new List<Card>();
+        
+        public void AddCard(DeckOfCards deck, string amountOfCards = "one")
+        {
+            List<Card> cards;
+
+            switch (amountOfCards)
+            {
+                case "one":
+                    if (deck.TryGetCard(out cards))
+                    {
+                        _cardInHand.AddRange(cards);
+                    }
+                    break;
+
+                default:
+                    Console.Write("Enter amount of cards: ");
+                    string userAmountOfCards = Console.ReadLine();
+
+                    if (Int32.TryParse(userAmountOfCards, out int userAmount))
+                    {
+                        while (userAmount-- > 0)
+                        {
+                            if (deck.TryGetCard(out cards))
+                            {
+                                _cardInHand.AddRange(cards);
+                            }
+                        }
+                    }
+                    break;
+            }
+        }
+
+        public void PrintAllCards()
+        {
+            foreach (Card card in _cardInHand)
+            {
+                switch (card.CardSuit)
+                {
+                    case "Hearts":
+                    case "Diamiond":
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"{card.CardSuit, -8} : {card.CardValue}");
+                        Console.ResetColor();
+                        break;
+
+                    default:
+                        Console.WriteLine($"{card.CardSuit, -8} : {card.CardValue}");
+                        break;
+                }
+            }
         }
     }
 
     internal class DeckOfCards
     {
-        private List<Card> _cards;
+        private List<Card> _cards = new List<Card>();
 
         public DeckOfCards()
         {
-            for (int i = 14; i > 0; i--)
+            AddCardsSuit("Spades");
+            AddCardsSuit("Hearts");
+            AddCardsSuit("Clubs");
+            AddCardsSuit("Diamonds");
+
+            ShuffleDeck();
+        }
+
+        private void AddCardsSuit(string suitName)
+        {
+            for (int i = 10; i > 0; i--)
             {
-                _cards.Add(new Card(i, "Spades"));
+                _cards.Add(new Card(i.ToString(), suitName));
             }
 
-            for (int i = 14; i > 0; i--)
+            _cards.Add(new Card("Jack", suitName));
+            _cards.Add(new Card("Queen", suitName));
+            _cards.Add(new Card("King", suitName));
+            _cards.Add(new Card("Ace", suitName));
+        }
+
+        public bool TryGetCard(out List<Card> cardOut, int amountOfCards = 1)
+        {
+            bool result = false;
+            cardOut = new List<Card>();
+
+            while (amountOfCards-- > 0)
             {
-                _cards.Add(new Card(i, "Hearts"));
+                if (_cards.Count > 0)
+                {
+                    cardOut.Add(_cards[0]);
+                    _cards.RemoveAt(0);
+                    result = true;
+                }
             }
 
-            for (int i = 14; i > 0; i--)
-            {
-                _cards.Add(new Card(i, "Clubs"));
-            }
-
-            for (int i = 14; i > 0; i--)
-            {
-                _cards.Add(new Card(i, "Diamonds"));
-            }
+            return result;
         }
 
         public void ShuffleDeck()
         {
-            Random rng = new Random();
-            int length = _cards.Count;
+            Random random = new Random();
 
-            while(length > 1)
+            for (int i = _cards.Count - 1; i >= 1; i--)
             {
-                length--;
-                int k = rng.Next(length + 1);
-                Card temp = _cards[k];
-                _cards[k] = _cards[length];
-                _cards[length] = temp;
+                int j = random.Next(i + 1);
+
+                Card temp = _cards[j];
+                _cards[j] = _cards[i];
+                _cards[i] = temp;
+            }
+        }
+
+        public void PrintAllCards()
+        {
+            foreach (Card card in _cards)
+            {
+                switch (card.CardSuit)
+                {
+                    case "Hearts":
+                    case "Diamonds":
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"{card.CardSuit, -8} : {card.CardValue}");
+                        Console.ResetColor();
+                        break;
+                    default:
+                        Console.WriteLine($"{card.CardSuit, -8} : {card.CardValue}");
+                        break;
+                }
             }
         }
     }
 
     internal class Card
     {
-        public int CardValue { get; private set; }
+        public string CardValue { get; private set; }
         public string CardSuit { get; private set; }
 
-        public Card(int cardValue, string cardSuit)
+        public Card(string cardValue, string cardSuit)
         {
             CardValue = cardValue;
             CardSuit = cardSuit;
